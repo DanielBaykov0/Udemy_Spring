ff4d9d1970030c1481f65438e81f30cd97f352e3
Daniel Baykov <danibaikov@abv.bg>
2022-12-25 18:11:56 +0200

    fixing some things

--- GuessTheNumberGame/core/src/main/java/org/example/Main.java
+++ GuessTheNumberGame/core/src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
