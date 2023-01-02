82719584a8727c1ce92bdd43632b12c3aebadfc0
Daniel Baykov <danibaikov@abv.bg>
2023-01-02 15:00:07 +0200

    adding lombok - complete

--- GuessTheNumberGame/core/src/main/java/baykov/daniel/NumberGeneratorImpl.java
+++ GuessTheNumberGame/core/src/main/java/baykov/daniel/NumberGeneratorImpl.java
@@ -1,14 +1,18 @@
 package baykov.daniel;
 
+import lombok.AccessLevel;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
+@Getter
 @Component
 public class NumberGeneratorImpl implements NumberGenerator{
 
     // == fields ==
+    @Getter(AccessLevel.NONE)
     private final Random random = new Random();
 
     private final int maxNumber;
@@ -27,15 +31,5 @@ public class NumberGeneratorImpl implements NumberGenerator{
     public int next() {
         // example min=5 max=20 --> max-min=15 --> range 0-15 + min --> 5-20
         return random.nextInt(maxNumber - minNumber) + minNumber;
-    }
-
-    @Override
-    public int getMaxNumber() {
-        return maxNumber;
-    }
-
-    @Override
-    public int getMinNumber() {
-        return minNumber;
     }
 }
