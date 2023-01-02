a814297dfd487740bcecb81aa3b889142b21b970
Daniel Baykov <danibaikov@abv.bg>
2023-01-02 21:38:22 +0200

    setting dispatcher servlet

---
+++ ToDoList/src/main/java/baykov/daniel/config/WebConfig.java
@@ -0,0 +1,11 @@
+package baykov.daniel.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "baykov.daniel")
+public class WebConfig {
+}
