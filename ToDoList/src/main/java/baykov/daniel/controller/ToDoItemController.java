e0ac27c25b6acd15fda8466bbc7069ec11d23234
Daniel Baykov <danibaikov@abv.bg>
2023-01-03 19:12:28 +0200

    implementing Add Item Feature and Post Redirect Get Pattern

--- ToDoList/src/main/java/baykov/daniel/controller/ToDoItemController.java
+++ ToDoList/src/main/java/baykov/daniel/controller/ToDoItemController.java
@@ -1,14 +1,22 @@
 package baykov.daniel.controller;
 
 import baykov.daniel.model.ToDoData;
+import baykov.daniel.model.ToDoItem;
 import baykov.daniel.service.ToDoItemService;
+import baykov.daniel.util.AttributeNames;
 import baykov.daniel.util.Mappings;
 import baykov.daniel.util.ViewNames;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDate;
+
+@Slf4j
 @Controller
 public class ToDoItemController {
 
@@ -32,5 +40,19 @@ public class ToDoItemController {
     @GetMapping(Mappings.ITEMS)
     public String items() {
         return ViewNames.ITEMS_LIST;
+    }
+
+    @GetMapping(Mappings.ADD_ITEM)
+    public String addEditItem(Model model) {
+        ToDoItem  toDoItem = new ToDoItem("", "", LocalDate.now());
+        model.addAttribute(AttributeNames.TODO_ITEM, toDoItem);
+        return ViewNames.ADD_ITEM;
+    }
+
+    @PostMapping(Mappings.ADD_ITEM)
+    public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) ToDoItem toDoItem) {
+        log.info("toDoItem from form = {}", toDoItem);
+        toDoItemService.addItem(toDoItem);
+        return "redirect:/" + Mappings.ITEMS;
     }
 }
--- ToDoList/src/main/java/baykov/daniel/util/ViewNames.java
+++ ToDoList/src/main/java/baykov/daniel/util/ViewNames.java
@@ -1,11 +1,12 @@
 package baykov.daniel.util;
 
-public class ViewNames {
+public final class ViewNames {
 
     // == constants ==
     public static final String ITEMS_LIST = "items_list";
+    public static final String ADD_ITEM = "add_item";
 
-    // == contructors ==
+    // == constructors ==
     private ViewNames() {
 
     }
